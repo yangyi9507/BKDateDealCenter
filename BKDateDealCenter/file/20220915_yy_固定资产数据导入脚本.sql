b6b8310a792ddd26e8a50b3d3e5a18afe05e26aa
建宾 潘 <1572902745@qq.com>
2022-09-15 16:51:31 +0800

    固定资产数据导入脚本

---
+++ BKDateDealCenter/file/20220915_yy_固定资产数据导入脚本.sql
@@ -0,0 +1,29 @@
+
+
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-001','运行中','VFP-40A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-002','运行中','VFP-40A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-003','运行中','VFP-40A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-004','运行中','VFP-32A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-005','运行中','VFP-32A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-006','运行中','VFP-32A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-007','运行中','VFP-32A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-008','运行中','VFP-32A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-009','运行中','VFP-32A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-010','运行中','FV-1600A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-011','运行中','VFX-1020A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-012','运行中','VFX-1020A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-013','运行中','VFP-32A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-014','运行中','VMP-32AⅡ','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-015','运行中','VMP-32AⅡ','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-016','运行中','VMP-32AⅡ','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-017','运行中','VMP-32AⅡ','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-018','运行中','Mazak加工中心','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-019','运行中','VMP-65A','加工中心');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-020','运行中','Mazak车床','数控车床');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-021','运行中','FTC260','数控车床');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-022','运行中','CAK4085Ni','数控车床');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-023','运行中','SK50P','数控车床');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-024','运行中','CAK4085Ni','数控车床');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-025','运行中','CA6136','普通车床');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-026','运行中','C6104','普通车床');
+INSERT INTO `fixedassets` ( `QCNumber`, `Status`, `Type`, `class`) VALUES ( 'BCJJG-027','运行中','XQ-1080','加工中心');
